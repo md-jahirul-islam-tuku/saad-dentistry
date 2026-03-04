bcff04202198854856e62229dc2443572e104709
Tuku <tukuwebian@gmail.com>
2026-03-04 03:10:29 +0300

    scrolling updated reagain

--- src/Pages/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -45,7 +45,7 @@ const ServiceDetails = () => {
       Swal.fire({
         icon: "success",
         title: "Deleted!",
-        timer: 1500,
+        timer: 2000,
         showConfirmButton: false,
       });
       queryClient.invalidateQueries(["reviews", _id]);
